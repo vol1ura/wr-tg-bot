f60a010d4dc36899b1ed017dc8ee1fb3420540c1
Yuriy Volodin <vol1ura@gmail.com>
2023-11-28 19:31:30 +0300

    small improvements

--- utils/chat_gpt.py
+++ utils/chat_gpt.py
@@ -1,9 +1,12 @@
 import g4f
 
 
-def ask(phrase: str):
-    return g4f.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=[{'role': 'user', 'content': phrase}],
-        timeout=100,
-    )
+def ask(phrase: str) -> str | None:
+    try:
+        return g4f.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[{'role': 'user', 'content': phrase}],
+            timeout=100,
+        )
+    except Exception:
+        return
